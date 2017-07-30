71d9c7df436aa99435c6eb98b8fe34a85aa5ece7
princesspretzel <brittanga@gmail.com>
2017-07-30 16:48:51 -0700

    add stars for insane level

--- stars.lua
+++ stars.lua
@@ -20,5 +20,41 @@ local level9 = '/images/level9start.png'
 table.insert(levelStars, level9)
 local level10 = '/images/level10start.png'
 table.insert(levelStars, level10)
+local level11 = '/images/level11start.png'
+table.insert(levelStars, level11)
+local level12 = '/images/level12start.png'
+table.insert(levelStars, level12)
+local level13 = '/images/level13start.png'
+table.insert(levelStars, level13)
+local level14 = '/images/level14start.png'
+table.insert(levelStars, level14)
+local level15 = '/images/level15start.png'
+table.insert(levelStars, level15)
+local level16 = '/images/level16start.png'
+table.insert(levelStars, level16)
+local level17 = '/images/level17start.png'
+table.insert(levelStars, level17)
+local level18 = '/images/level18start.png'
+table.insert(levelStars, level18)
+local level19 = '/images/level19start.png'
+table.insert(levelStars, level19)
+local level20 = '/images/level20start.png'
+table.insert(levelStars, level20)
+local level21 = '/images/level21start.png'
+table.insert(levelStars, level21)
+local level22 = '/images/level22start.png'
+table.insert(levelStars, level22)
+local level23 = '/images/level23start.png'
+table.insert(levelStars, level23)
+local level24 = '/images/level24start.png'
+table.insert(levelStars, level24)
+local level25 = '/images/level25start.png'
+table.insert(levelStars, level25)
+local level26 = '/images/level26start.png'
+table.insert(levelStars, level26)
+local level27 = '/images/level27start.png'
+table.insert(levelStars, level27)
+local level28 = '/images/level28start.png'
+table.insert(levelStars, level28)
 
 return levelStars
