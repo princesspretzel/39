91641c3b2eab833a845c586a05c599a0b51970b3
princesspretzel <brittanga@gmail.com>
2017-07-29 20:01:17 -0700

    fixed messed-up formatting

--- main.lua
+++ main.lua
@@ -5,95 +5,95 @@ local Star = require('star')
 lights = { }
 
 function gameReset()
-	poweringDown = false
-	doneFlashing = false
-	playerTurn = false
-	lost = false
-	won = false
+  poweringDown = false
+  doneFlashing = false
+  playerTurn = false
+  lost = false
+  won = false
 end
 
 -- convert lines of lights to number of bulbs
 function levelsToLightBulbs(levelNum)
-	local total = 0
-	for n = 1, levelNum do
-		total = total + n
-		n = n + 1
-	end
-	return total
+  local total = 0
+  for n = 1, levelNum do
+    total = total + n
+    n = n + 1
+  end
+  return total
 end
 
 -- create the tree shape
 function stringLights(num)
-	-- hardcode alert (radius-aware)
+  -- hardcode alert (radius-aware)
   xBase = (gameWidth / 2) - 10
   yBase = 200
   for row = 1, num do
-  	xBase = xBase - 20
-  	local x = xBase
-  	local y = yBase
-  	for col = 1, row do
-  		-- hardcode alert (https://stackoverflow.com/questions/17295861/how-to-create-concentric-triangles-with-cairo)
-  		y = y + 10
-  		x = x + 30
-	    local light = Light(x, y, ((levelsToLightBulbs(row-1))+col))
-	    table.insert(lights, light)
-	    col = col + 1
-   	end
-   	yBase = yBase + 40
-   	row = row + 1
+    xBase = xBase - 20
+    local x = xBase
+    local y = yBase
+    for col = 1, row do
+      -- hardcode alert (https://stackoverflow.com/questions/17295861/how-to-create-concentric-triangles-with-cairo)
+      y = y + 10
+      x = x + 30
+      local light = Light(x, y, ((levelsToLightBulbs(row-1))+col))
+      table.insert(lights, light)
+      col = col + 1
+    end
+    yBase = yBase + 40
+    row = row + 1
   end
 end
 
 function love.load()
-	gameWidth, gameHeight = love.graphics.getDimensions()
-	won = false
-	lost = false
+  gameWidth, gameHeight = love.graphics.getDimensions()
+  won = false
+  lost = false
 
-	-- io.write(' ')love.graphics.printf(text, 0, 0, love.graphics.getWidth())
-	-- io.write('Choose a level from 3 to 10: ')
-	-- level = io.read()
-	local level = 7
-	local guesses = 3
-	stringLights(level)
+  -- io.write(' ')love.graphics.printf(text, 0, 0, love.graphics.getWidth())
+  -- io.write('Choose a level from 3 to 10: ')
+  -- level = io.read()
+  local level = 7
+  local guesses = 3
+  stringLights(level)
 
-	currentRound = Round(level, guesses)
+  currentRound = Round(level, guesses)
   currentRound:generate(levelsToLightBulbs(level), guesses)
 
-	local starFile = '/images/basestart.png'
-	local starImage = love.graphics.newImage(starFile)
-	local starWidth, starHeight = starImage:getDimensions( )
+  local starFile = '/images/basestart.png'
+  local starImage = love.graphics.newImage(starFile)
+  local starWidth, starHeight = starImage:getDimensions( )
   star = Star(starImage, starWidth, starHeight)
 end
 
 function love.draw()
-	star:draw()
-	for idx, light in ipairs(lights) do
-		light:draw()
-	end
+  star:draw()
+  for idx, light in ipairs(lights) do
+    light:draw()
+  end
 end
 
 function love.keypressed(key)
-	if key == "escape" then
-		love.event.quit()
-	end
-	if key == "return" then
-		gameReset()
-		love.event.quit("restart")
-	end
+  if key == "escape" then
+    love.event.quit()
+  end
+  if key == "return" then
+    gameReset()
+    love.event.quit("restart")
+  end
 end
 
 function love.mousepressed(x, y, button, istouch)
-	if button == 1 and (currentRound.playerTurn == true) then
-		for idx, light in ipairs(lights) do
-			light:mouseCollision(x, y)
-		end
-	end
+  if button == 1 and (currentRound.playerTurn == true) then
+    for idx, light in ipairs(lights) do
+      light:mouseCollision(x, y)
+    end
+  end
 end
 
 function love.update(dt)
-	currentRound:update(dt)
-	star:update(dt)
-	for idx, light in ipairs(lights) do
-		light:update(dt)
-	end
+  currentRound:update(dt)
+  star:update(dt)
+  for idx, light in ipairs(lights) do
+    light:update(dt)
+  end
 end
